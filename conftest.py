3a80aa859d4c2484c6f32ff8db805c47c351095e
Daniel Andrlik <daniel@andrlik.org>
2024-12-16 15:45:06 -0500

    Initial commit :tada:

---
+++ conftest.py
@@ -0,0 +1,6 @@
+# conftest.py
+#
+# Copyright (c) 2024 Daniel Andrlik
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
---
+++ src/podcast_charts/admin.py
@@ -0,0 +1,8 @@
+# admin.py
+#
+# Copyright (c) 2024 Daniel Andrlik
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Admin registration objects for podcast_charts"""
